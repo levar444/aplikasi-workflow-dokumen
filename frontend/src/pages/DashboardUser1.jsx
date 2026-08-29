167366c66520e136f53c3defce7ddcff54e62168
levar444 <damdj5994@gmail.com>
2026-08-29 11:54:11 +0700

    Menambahkan pintasan dashboard user 6 (operator) ke AdminDashboard

--- frontend/src/pages/DashboardUser1.jsx
+++ frontend/src/pages/DashboardUser1.jsx
@@ -193,7 +193,7 @@ export default function DashboardUser1() {
           <div style={{ width: '100%', overflowX: 'auto' }}>
             <table style={{ width: '100%', borderCollapse: 'collapse', minWidth: '950px' }}>
               <thead>
-                <tr style={{ backgroundColor: '#9ce78e', textAlign: 'left' }}>
+                <tr style={{ backgroundColor: '#ade9a2', textAlign: 'left' }}>
                   <th style={{ padding: '12px', borderBottom: '1px solid #e5e7eb', fontSize: '13px' }}>No. Dokumen</th>
                   <th style={{ padding: '12px', borderBottom: '1px solid #e5e7eb', fontSize: '13px' }}>Judul</th>
                   <th style={{ padding: '12px', borderBottom: '1px solid #e5e7eb', fontSize: '13px' }}>Asal File (Pembuat)</th>
